7b5e97feb990f1fd19372dcb3fae572f927c7519
b1mind <brent@1mindsolutions.com>
2021-09-02 15:29:06 -0600

    🧪 adding files for ghpages subtree deploy

---
+++ .svelte-kit/build/app.js
@@ -0,0 +1,130 @@
+import { respond } from '@sveltejs/kit/ssr';
+import root from './generated/root.svelte';
+import { set_paths, assets } from './runtime/paths.js';
+import { set_prerendering } from './runtime/env.js';
+import * as user_hooks from "../../src/hooks.js";
+
+const template = ({ head, body }) => "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<meta charset=\"utf-8\" />\n\t\t<link rel=\"icon\" href=\"/favicon.png\" />\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n\n\t\t" + head + "\n\t</head>\n\t<body>\n\t\t<div id=\"svelte\">" + body + "</div>\n\t</body>\n</html>\n";
+
+let options = null;
+
+const default_settings = { paths: {"base":"","assets":""} };
+
+// allow paths to be overridden in svelte-kit preview
+// and in prerendering
+export function init(settings = default_settings) {
+	set_paths(settings.paths);
+	set_prerendering(settings.prerendering || false);
+
+	const hooks = get_hooks(user_hooks);
+
+	options = {
+		amp: false,
+		dev: false,
+		entry: {
+			file: assets + "/_app/start-b3d4fc19.js",
+			css: [assets + "/_app/assets/start-61d1577b.css"],
+			js: [assets + "/_app/start-b3d4fc19.js",assets + "/_app/chunks/vendor-a4e104ac.js"]
+		},
+		fetched: undefined,
+		floc: false,
+		get_component_path: id => assets + "/_app/" + entry_lookup[id],
+		get_stack: error => String(error), // for security
+		handle_error: (error, request) => {
+			hooks.handleError({ error, request });
+			error.stack = options.get_stack(error);
+		},
+		hooks,
+		hydrate: true,
+		initiator: undefined,
+		load_component,
+		manifest,
+		paths: settings.paths,
+		prerender: true,
+		read: settings.read,
+		root,
+		service_worker: null,
+		router: true,
+		ssr: true,
+		target: "#svelte",
+		template,
+		trailing_slash: "never"
+	};
+}
+
+const d = decodeURIComponent;
+const empty = () => ({});
+
+const manifest = {
+	assets: [{"file":"favicon.png","size":1571,"type":"image/png"},{"file":"robots.txt","size":67,"type":"text/plain"},{"file":"svelte-welcome.png","size":360807,"type":"image/png"},{"file":"svelte-welcome.webp","size":115470,"type":"image/webp"}],
+	layout: "src/routes/__layout.svelte",
+	error: ".svelte-kit/build/components/error.svelte",
+	routes: [
+		{
+						type: 'page',
+						pattern: /^\/$/,
+						params: empty,
+						a: ["src/routes/__layout.svelte", "src/routes/index.svelte"],
+						b: [".svelte-kit/build/components/error.svelte"]
+					},
+		{
+						type: 'page',
+						pattern: /^\/about\/?$/,
+						params: empty,
+						a: ["src/routes/__layout.svelte", "src/routes/about.svelte"],
+						b: [".svelte-kit/build/components/error.svelte"]
+					},
+		{
+						type: 'endpoint',
+						pattern: /^\/todos\.json$/,
+						params: empty,
+						load: () => import("../../src/routes/todos/index.json.js")
+					},
+		{
+						type: 'page',
+						pattern: /^\/todos\/?$/,
+						params: empty,
+						a: ["src/routes/__layout.svelte", "src/routes/todos/index.svelte"],
+						b: [".svelte-kit/build/components/error.svelte"]
+					},
+		{
+						type: 'endpoint',
+						pattern: /^\/todos\/([^/]+?)\.json$/,
+						params: (m) => ({ uid: d(m[1])}),
+						load: () => import("../../src/routes/todos/[uid].json.js")
+					}
+	]
+};
+
+// this looks redundant, but the indirection allows us to access
+// named imports without triggering Rollup's missing import detection
+const get_hooks = hooks => ({
+	getSession: hooks.getSession || (() => ({})),
+	handle: hooks.handle || (({ request, resolve }) => resolve(request)),
+	handleError: hooks.handleError || (({ error }) => console.error(error.stack)),
+	externalFetch: hooks.externalFetch || fetch
+});
+
+const module_lookup = {
+	"src/routes/__layout.svelte": () => import("../../src/routes/__layout.svelte"),".svelte-kit/build/components/error.svelte": () => import("./components/error.svelte"),"src/routes/index.svelte": () => import("../../src/routes/index.svelte"),"src/routes/about.svelte": () => import("../../src/routes/about.svelte"),"src/routes/todos/index.svelte": () => import("../../src/routes/todos/index.svelte")
+};
+
+const metadata_lookup = {"src/routes/__layout.svelte":{"entry":"pages/__layout.svelte-3db71b96.js","css":["assets/pages/__layout.svelte-592d11be.css"],"js":["pages/__layout.svelte-3db71b96.js","chunks/vendor-a4e104ac.js"],"styles":[]},".svelte-kit/build/components/error.svelte":{"entry":"error.svelte-69804e1b.js","css":[],"js":["error.svelte-69804e1b.js","chunks/vendor-a4e104ac.js"],"styles":[]},"src/routes/index.svelte":{"entry":"pages/index.svelte-f7bcde44.js","css":["assets/pages/index.svelte-39d30dba.css"],"js":["pages/index.svelte-f7bcde44.js","chunks/vendor-a4e104ac.js"],"styles":[]},"src/routes/about.svelte":{"entry":"pages/about.svelte-4b5a0bae.js","css":["assets/pages/about.svelte-bf4528fa.css"],"js":["pages/about.svelte-4b5a0bae.js","chunks/vendor-a4e104ac.js"],"styles":[]},"src/routes/todos/index.svelte":{"entry":"pages/todos/index.svelte-4c3efe5c.js","css":["assets/pages/todos/index.svelte-784042c1.css"],"js":["pages/todos/index.svelte-4c3efe5c.js","chunks/vendor-a4e104ac.js"],"styles":[]}};
+
+async function load_component(file) {
+	const { entry, css, js, styles } = metadata_lookup[file];
+	return {
+		module: await module_lookup[file](),
+		entry: assets + "/_app/" + entry,
+		css: css.map(dep => assets + "/_app/" + dep),
+		js: js.map(dep => assets + "/_app/" + dep),
+		styles
+	};
+}
+
+export function render(request, {
+	prerender
+} = {}) {
+	const host = request.headers["host"];
+	return respond({ ...request, host }, options, { prerender });
+}
