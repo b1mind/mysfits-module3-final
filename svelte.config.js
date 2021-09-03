9daf9b593a1d02b2776cdccf13ffec6c2472b33d
b1mind <brent@1mindsolutions.com>
2021-09-03 16:58:30 -0600

    🧪Test: vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
-const config = {
+export default {
   kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    paths: {
-      base: '/mysfits-module3-final'
-    },
-    appDir: 'files'
+    adapter: vercel()
   }
 };
-
-export default config;
