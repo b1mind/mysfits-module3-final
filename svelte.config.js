1ebbfc733870f0cabbfcd49b9a1079c51336de99
b1mind <brent@1mindsolutions.com>
2021-09-03 18:32:50 -0600

    📦 Added Sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
 import vercel from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
 
 export default {
   kit: {
     adapter: vercel()
-  }
+  },
+  preprocess: preprocess
 };
