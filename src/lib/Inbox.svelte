51313f5b441193b940590bf80c53ff7e448976c2
b1mind <brent@1mindsolutions.com>
2021-09-18 22:27:12 -0600

    🧙‍♂️added: chevron svg to buttons

--- src/lib/Inbox.svelte
+++ src/lib/Inbox.svelte
@@ -10,7 +10,12 @@
     <form class="flex" on:submit|preventDefault>
       <label for="name">Name<input type="text" /></label>
       <label>Email<input type="text" /></label>
-      <button class="btn">Subscribe</button>
+      <button class="btn"
+        >Subscribe
+        <svg class="btn-icon" width="18" height="18" viewBox="0 0 24 24">
+          <path d="M9.984 6l6 6-6 6-1.406-1.406 4.594-4.594-4.594-4.594z" />
+        </svg>
+      </button>
     </form>
   </div>
 </section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,7 +16,12 @@
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, dicta
       nostrum! Dicta beatae quo consequuntur inventore provident magni.
     </p>
-    <button class="btn">connect ></button>
+    <button class="btn"
+      >Connect
+      <svg class="btn-icon" width="18" height="18" viewBox="0 0 24 24">
+        <path d="M9.984 6l6 6-6 6-1.406-1.406 4.594-4.594-4.594-4.594z" />
+      </svg>
+    </button>
   </div>
 </section>
 
