50662de86905b267416ccfcc04abd1744284b68e
b1mind <brent@1mindsolutions.com>
2021-09-23 21:23:56 -0600

    👗 styled: form inputs/labels

--- src/lib/Inbox.svelte
+++ src/lib/Inbox.svelte
@@ -3,13 +3,14 @@
     <h2>Inbox</h2>
     <div class="title">Knowledge letters</div>
 
-    <div class="img">
+    <div class="img space-lg">
       <img src="/img/newsletter.jpg" alt="Subscribe for newsletter" />
     </div>
 
     <form class="flex" on:submit|preventDefault>
-      <label for="name">Name<input type="text" /></label>
-      <label>Email<input type="text" /></label>
+      <label for="name">Name<input type="text" placeholder="Your Name" /></label
+      >
+      <label>Email<input type="text" placeholder="email@domain.com" /></label>
       <button class="btn"
         >Subscribe
         <svg class="btn-icon" width="18" height="18" viewBox="0 0 24 24">
@@ -36,9 +37,25 @@
   label,
   input {
     width: 90%;
+    padding: 0.3em;
+  }
+
+  label {
+    font-size: var(--fs-200);
   }
 
   input {
     display: block;
+    font-size: var(--fs-400);
+    border: none;
+    border-bottom: 1px solid var(--clr-neutral-300);
+    border-radius: 0;
+
+    &:focus {
+      outline: none;
+    }
+    &::placeholder {
+      color: var(--clr-neutral-300);
+    }
   }
 </style>
