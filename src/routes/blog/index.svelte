d4b937d123a4fd7e8fa4982bf8ef494bfe8543ab
b1mind <brent@1mindsolutions.com>
2021-09-25 15:17:29 -0600

    ➕ added: Blog nav

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -26,6 +26,27 @@
 
 <section>
   <div class="container">
+    <header class="flex">
+      <nav>
+        <ul>
+          <li><a href="#void">Design</a></li>
+          <li><a href="#void">Front-End</a></li>
+          <li><a href="#void">Back-End</a></li>
+          <li><a href="#void">Web</a></li>
+          <li><a href="#void">Apps</a></li>
+        </ul>
+      </nav>
+      <button class="btn"
+        >Subscribe
+
+        <svg class="btn-icon" width="18" height="18" viewBox="0 0 24 24">
+          <path d="M9.984 6l6 6-6 6-1.406-1.406 4.594-4.594-4.594-4.594z" />
+        </svg>
+      </button>
+    </header>
+  </div>
+
+  <div class="container">
     <div class="grid">
       {#each posts as { id, title, body }}
         <div class="card">
@@ -41,10 +62,27 @@
     <a href="#void"> More Articles </a>
   </section>
 </div>
+
 <Inbox />
 
 <style lang="scss">
   @use '../../lib/scss/vars' as *;
+  header {
+    padding: 2rem;
+    align-items: center;
+
+    @media (max-width: $mediaSml) {
+      display: none;
+      visibility: hidden;
+    }
+  }
+
+  section:first-child {
+    @media (min-width: $mediaSml) {
+      padding-top: 0;
+    }
+  }
+
   .boarder {
     text-align: center;
   }
@@ -55,7 +93,7 @@
 
   @media (min-width: $mediaMed) {
     .card:first-child {
-      grid-column: 1 / 4;
+      grid-column: 1 / -1;
     }
 
     .card:nth-child(5n + 3) {
